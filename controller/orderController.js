cbcf59b1868aad835b738051cd36e01a2de3e552
Dinuka Liyanage <infodinukalk@gmail.com>
2025-11-12 02:17:57 +0530

    refactor(orderController): clean up code formatting for better readability

--- controller/orderController.js
+++ controller/orderController.js
@@ -8,19 +8,21 @@ import {
   searchOrderRecord,
 } from "../model/orderModel.js";
 
+import { getAllCustomers } from "../model/customerModel.js";
+import { getAllItems } from "../model/itemModel.js";
 
 //handle place order btn
 $("#btnPlaceOrder").on("click", function () {
-  $('#order_header_section').hide();
-   $("#orderListContainer").hide();
-   $("#placeOrderContainer").show();
-   
+  $("#order_header_section").hide();
+  $("#orderListContainer").hide();
+  $("#placeOrderContainer").show();
 
+  populateOrderDropdowns();
 });
 
 //handle back to list btn
 $("#btnBackToList").on("click", function () {
-  $('#order_header_section').show();
-   $("#placeOrderContainer").hide();
-   $("#orderListContainer").show();
+  $("#order_header_section").show();
+  $("#placeOrderContainer").hide();
+  $("#orderListContainer").show();
 });
